637709749d2422593fa909d0c140d0a9df087bbb
Joseph J <jerkjoe61@gmail.com>
2022-11-26 12:01:18 -0800

    alllow cors

--- netlify/functions/auth/users.js
+++ netlify/functions/auth/users.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONs'
 };
 
 const uri = "mongodb+srv://jerkjoe:UkAoBt4swcR5YtJ7@cluster0.t5mtc.mongodb.net/kanban?retryWrites=true&w=majority";
